f8cca85d037d7ab1f6e1a609ecb2ffc2c757e76d
sameer karn <nischalkarn369369@gmail.com>
2024-02-19 00:08:36 +0200

    Updated

--- src/App.js
+++ src/App.js
@@ -1,10 +1,28 @@
-import React from 'react'
-import Homepage from './Components/Homepage';
+import React from "react";
+import { ColorModeContext, useMode } from "./theme";
+import { CssBaseline, ThemeProvider } from "@mui/material";
+import { Route, Routes, Navigate } from "react-router-dom";
+import Admin from "./Components/Admin";
+import Homepage from "./Components/Homepage";
 
 const App = () => {
+  const [theme, colorMode] = useMode();
   return (
-   <Homepage/>
-  )
-}
+    <ColorModeContext.Provider value={colorMode}>
+      <ThemeProvider theme={theme}>
+        <CssBaseline />
+        {/* making socket globally accessible */}
+        <div className="app">
+          <main >
+            <Routes>
+              <Route exact path="/" element={<Homepage/>} />
+              <Route path="*" element={<Navigate to="/" />} />
+            </Routes>
+          </main>
+        </div>
+      </ThemeProvider>
+    </ColorModeContext.Provider>
+  );
+};
 
-export default App
+export default App;
